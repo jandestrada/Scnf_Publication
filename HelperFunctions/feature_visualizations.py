1231610bd229a32cda6befbbca9005ea080182ad
Jan Estrada <janestrada7@gmail.com>
2019-07-10 14:31:51 -0400

    updated noteboks and added an EEHEE protein file in case protein visualization is done

--- HelperFunctions/feature_visualizations.py
+++ HelperFunctions/feature_visualizations.py
@@ -251,9 +251,13 @@ def loo_plot(tuple_with_list_and_metric):
             
             
 #################### HELPER TO HELPER FUNCTIONS #######################
-def get_count_percentiles_list(df):
-    total_n_features = len(df)
-    percentiles = [.65,.5,.25,.15]
+def get_count_percentiles_list(df,percentiles=[.65,.50,.25,.15]):
+    """
+    Returns a list of the amount of entropy features in a given percentile 
+    Assumes importance values are non-repeating and index of dataframe is 
+    sorted by importance. 
+    """
+    
     spc_features = [
         'S_PC', 'Mean_H_entropy', 'Mean_L_entropy', 'Mean_E_entropy', 
         'Mean_res_entropy', 'SumH_entropies', 'SumL_entropies', 'SumE_entropies', 
@@ -261,14 +265,17 @@ def get_count_percentiles_list(df):
         'L_min_entropy', 'L_range_entropy', 'E_max_entropy', 'E_min_entropy', 
         'E_range_entropy']
     count_percentiles_list = []
-
+    unique_importances = df['Importance'].unique()
     for i in range(len(percentiles)):
-        #get the number of features in a given percentile
-        n_percentile = round(total_n_features*percentiles[i])
-
-        #print(n_percentile)
-        #get subset of dataframe in that percentile
-        df_percentile  = df.iloc[:n_percentile,:]
+        
+        #this gives the inx of the cutoff value for the percentile
+        min_val_inx = round(percentiles[i]*len(unique_importances))
+        
+        #the cutoff value for the percentile
+        min_val = unique_importances[min_val_inx]
+        
+        #get the subset of rows that make the percentile cutoff
+        df_percentile = df[df['Importance']>= min_val]
 
         #get number of entropy features in subset
         count_percentile = len(
