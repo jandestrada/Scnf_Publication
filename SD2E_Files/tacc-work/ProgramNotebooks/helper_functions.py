4cd6c27fc044e8df30ab3289b1dc6589c02bac58
Jan Estrada <janestrada7@gmail.com>
2020-02-07 15:32:25 -0500

    added Tacc Notebooks

---
+++ SD2E_Files/tacc-work/ProgramNotebooks/helper_functions.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3e2a0d03c88047afc59efb5c154962df626aa8aabeb2a57bd8658a893c080cd7
+size 9988
---
+++ SD2E_Files/tacc-work/ProgramNotebooks/stability_scores_tsv.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6c6646732511f2297d4a760b4190bca7cef35aec3f29bd24309926efe2d5c8bf
+size 2534
